a7754668b29ad1e4f4a20ab10534c5f35334084c
Gaston Figueroa <gastonfig@gmail.com>
2013-12-31 14:46:50 -0800

    -n Added version json doc.

---
+++ lg-version.js
@@ -0,0 +1,12 @@
+jsonCallback(
+    {
+        "versionInfo":
+        
+            {
+                "extensionVersion": "0.8.0",
+                "extensionName": "Layer Guides",
+                "versionDate": "12-31-13"
+            }
+        
+    }
+);
